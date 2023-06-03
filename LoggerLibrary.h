76282b574dcd83e888a89af64e044e454bb34e29
GrKoR <dr.g.kornev@gmail.com>
2023-06-04 00:14:20 +0400

    upd: print() for HEX array + refactoring

--- LoggerLibrary.h
+++ LoggerLibrary.h
@@ -16,28 +16,30 @@
     #warning 'Serial' is used for debugging!
 #endif // HAL_UART_MODULE_ENABLED
 
+enum logger_output_type_t
+{
+    LOG_OUT_TYPE_NONE = 0x00,
+    LOG_OUT_TYPE_HEX = 0x01,
+    LOG_OUT_TYPE_BYTES = 0x02,
+};
+
 template <uint16_t buffer_length = 256>
 class DebugSerial
 {
 public:
     const char *DebugTopic = "DEBUG";
 
-    DebugSerial() {};
-
-    DebugSerial &Print(const char *str, const char *topic = nullptr)
+    DebugSerial()
     {
-        PrintTopic(topic);
-        _HW_Print((uint8_t *)str, strlen(str));
-        
-        return *this;
+        _ResetBuffer();
     }
 
-    DebugSerial &Print(const uint8_t *str, uint16_t length, const char *topic = nullptr)
+    DebugSerial &PrintTopic(const char *topic)
     {
-        PrintTopic(topic);
-        _HW_Print((uint8_t *)str, length);
+        if (topic == nullptr)
+            return *this;
 
-        return *this;
+        return Printf("+%s\t", topic);
     }
 
     DebugSerial &Printf(const char *str, ...)
@@ -50,9 +52,77 @@ public:
         vsnprintf(_buffer, buffer_length, str, argptr);
         va_end(argptr);
 
-        _HW_Print((uint8_t *)_buffer, strlen(_buffer));
+        _Print(_buffer, strlen(_buffer));
 
         return *this;
+    }
+
+    DebugSerial &Print(const void *data, uint16_t data_length, const char *topic, logger_output_type_t data_type)
+    {
+        if (data == nullptr || data_length == 0)
+            return *this;
+
+        if (topic != nullptr)
+        {
+            _FillTopic(topic);
+        }
+
+        switch (data_type)
+        {
+        case LOG_OUT_TYPE_HEX:
+        {
+            int result = 0;
+            for (uint8_t i = 0; i < data_length; i++)
+            {
+                result = snprintf(_buffer_ptr, _buffer_size_left, "0x%02X%s", ((uint8_t *)data)[i], (i == data_length - 1) ? "" : ", ");
+                if (result < 0)
+                    break;
+                
+                _buffer_ptr += result;
+                _buffer_size_left -= result;
+            }
+            _Print(_buffer, buffer_length - _buffer_size_left + 1);
+            
+            return *this;
+        }
+        
+        case LOG_OUT_TYPE_BYTES:
+        {
+            _Print(_buffer, buffer_length - _buffer_size_left + 1);
+            _Print(data, data_length);
+            
+            return *this;
+        }
+
+        default:
+            return *this;
+        }
+    }
+
+    DebugSerial &Print(const char *str)
+    {
+        if (str == nullptr)
+            return *this;
+        
+        return Print(str, strlen(str), nullptr, LOG_OUT_TYPE_BYTES);
+    }
+
+    DebugSerial &Print(const char *str, const char *topic)
+    {
+        if (str == nullptr)
+            return *this;
+        
+        return Print(str, strlen(str), topic, LOG_OUT_TYPE_BYTES);
+    }
+
+    DebugSerial &Print(const void *data, uint16_t data_length, logger_output_type_t data_type = LOG_OUT_TYPE_BYTES)
+    {
+        return Print(data, data_length, nullptr, data_type);
+    }
+
+    DebugSerial &Print(const void *data, uint16_t data_length, const char *topic)
+    {
+        return Print(data, data_length, topic, LOG_OUT_TYPE_BYTES);
     }
 
     DebugSerial &PrintNewLine()
@@ -60,21 +130,41 @@ public:
         return Print("\n");
     }
 
-    DebugSerial &PrintTopic(const char *topic)
-    {
-        if (topic == nullptr)
-            return *this;
-
-        return Printf("+%s\t", topic);
-    }
-
 private:
     char _buffer[buffer_length];
+    char *_buffer_ptr = _buffer;
+    uint16_t _buffer_size_left = buffer_length;
+
+    void _ResetBuffer()
+    {
+        memset(_buffer, 0, buffer_length);
+        _buffer_ptr = _buffer;
+        _buffer_size_left = buffer_length;
+    }
+
+    int _FillTopic(const char *topic)
+    {
+        int result = snprintf(_buffer_ptr, _buffer_size_left, "+%s\t", topic);
+
+        if (result > 0)
+        {
+            _buffer_ptr += result;
+            _buffer_size_left -= result;
+        }
+
+        return result;
+    }
+
+    void _Print(const void *pData, uint16_t Size)
+    {
+        _HW_Print(pData, Size);
+        _ResetBuffer();
+    }
 
 #ifdef HAL_UART_MODULE_ENABLED
-    bool _HW_Print(uint8_t *pData, uint16_t Size)
+    bool _HW_Print(const void *pData, uint16_t Size)
     {
-        HAL_StatusTypeDef result = HAL_UART_Transmit(&hDebugUart, pData, Size, 64);
+        HAL_StatusTypeDef result = HAL_UART_Transmit(&hDebugUart, (uint8_t *)pData, Size, 64);
         if (result != HAL_OK)
         {
             HAL_UART_AbortTransmit(&hDebugUart);
@@ -84,15 +174,15 @@ private:
     }
 
 #elif defined(ESP32) // HAL_UART_MODULE_ENABLED
-    bool _HW_Print(uint8_t *pData, uint16_t Size)
+    bool _HW_Print(const void *pData, uint16_t Size)
     {
-        Serial.write(pData, Size);
+        Serial.write((uint8_t *)pData, Size);
 
         return true;
     }
 
 #elif defined(_WIN32) || defined(_WIN64) || defined(__linux__) // ESP32
-    bool _HW_Print(uint8_t *pData, uint16_t Size)
+    bool _HW_Print(const void *pData, uint16_t Size)
     {
         std::cout << std::string((char *)pData, Size) << std::flush;
 
@@ -101,7 +191,7 @@ private:
 
 #else // HAL_UART_MODULE_ENABLED
 #warning All logging will be ignored
-    bool _HW_Print(uint8_t *pData, uint16_t Size)
+    bool _HW_Print(const void *pData, uint16_t Size)
     {
         return false;
     }
